a31c0d541f0e7f191a5bb89a4f2f7fe0e74dc013
PGabriel-MB <pablogrbarbosa@gmail.com>
2020-09-04 00:32:15 -0300

    classe finalizada Passando pra a fase de fragmentação da classe

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,3 +1,4 @@
+import './Contador.css';
 import React from 'react';
 
 export default class Contador extends React.Component {
@@ -7,24 +8,36 @@ export default class Contador extends React.Component {
         valor: this.props.valor || 0
     }
 
-    inc(){
-
+    inc = () => {
+        this.setState({
+            valor: this.state.valor + this.state.passo
+        })
     }
 
-    dec() {
-
+    dec = () => {
+        this.setState({
+            valor: this.state.valor - this.state.passo
+        })
     }
 
     render() {
         return (
-            <div>
+            <div className="Contador">
                 <h2>Contador</h2>
-                <h4>Passo: {this.state.passo}</h4>
+
+                <div>
+                    <label for="passoInput"> Passo: </label>
+                    <input id="passoInput" 
+                        type="number" 
+                        value={this.state.passo} 
+                        onChange={e => this.setState({ passo: +e.target.value })} />
+                </div>
+
                 <h4>Valor: {this.state.valor}</h4>
 
                 <div>
-                    <button> + </button>
-                    <button> - </button>
+                    <button onClick={this.inc}> + </button>
+                    <button onClick={this.dec}> - </button>
                 </div>
             </div>
         );
